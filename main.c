a2f0906d0bef4d05bee77055a2b1bddd414a9033
JoaksIPN <joaks.jdm@gmail.com>
2017-11-07 21:37:25 -0600

    indete

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include"presentacion.h"
 
 /*
-AUTOR: Dominguez Moran Joaquín, García Ibañez Diego, Rosas Sánchez Paloma. Agosto 2017.
+AUTOR: Dominguez Moran Joaquín, García Ibañez Diego, Rosas Sánchez Paloma. Noviembre 2017.
 VERSIÓN: 1.0.
 
 DESCRIPCIÓN: Programa que calcula el elemento n de Fibonnaci y Tribonacci.
@@ -33,30 +33,32 @@ DESCRIPCIÓN: Calcula el elemento n de Fibonnacci Recursivamente.
 ENTRADA: El elemento n que se desea calcular.
 SALIDA: El valor en la sucesion de Fibonacci del elemento.
 */
-u Fibonacci( int n)
-	if(n==0)
-	{
-		elementosFibonacci[n]=0;
-		return elementosFibonacci[n] ;
-	}
+u
+Fibonacci (int n)
+{
+  if (n == 0)
+    {
+      elementosFibonacci[n] = 0;
+      return elementosFibonacci[n];
+    }
 
-	if(n==1)
-	{
-		elementosFibonacci[n]=1;
-		return elementosFibonacci[n] ;
-	}
+  if (n == 1)
+    {
+      elementosFibonacci[n] = 1;
+      return elementosFibonacci[n];
+    }
 
-	if (elementosFibonacci[n]!=0)
-	{
-		return elementosFibonacci[n];
-	}
+  if (elementosFibonacci[n] != 0)
+    {
+      return elementosFibonacci[n];
+    }
 
-	else
-	{
-		elementosFibonacci[n] = Fibonacci(n-2) + Fibonacci(n-1);
-		return elementosFibonacci[n];
-	}
-	
+  else
+    {
+      elementosFibonacci[n] = Fibonacci (n - 2) + Fibonacci (n - 1);
+      return elementosFibonacci[n];
+    }
+
 };
 
 /*
@@ -66,115 +68,120 @@ ENTRADA: El elemento n que se desea calcular.
 SALIDA: El valor en la sucesion de Tribonacci del elemento.
 */
 
-u Tribonacci(int n)
+u
+Tribonacci (int n)
 {
-	if(n==0)//Condicional que evalua los casos base anteriores
-	{
-		elementosTribonacci[n]=1;
-		return elementosTribonacci[n] ;
-	}
-	
-	if(n==1)//Condicional que evalua los casos base anteriores
-	{
-		elementosTribonacci[n]=1;
-		return elementosTribonacci[n] ;
-	}
-	
-	if(n==2)//Condicional que evalua los casos base anteriores
-	{
-		elementosTribonacci[n]=2;
-		return elementosTribonacci[n] ;
-	}
+  if (n == 0)			//Condicional que evalua los casos base anteriores
+    {
+      elementosTribonacci[n] = 1;
+      return elementosTribonacci[n];
+    }
 
-	if (elementosTribonacci[n]!=0)
-	{
-		return elementosTribonacci[n];
-	}
+  if (n == 1)			//Condicional que evalua los casos base anteriores
+    {
+      elementosTribonacci[n] = 1;
+      return elementosTribonacci[n];
+    }
 
-	else
-	{
-		elementosTribonacci[n] = Tribonacci(n-1)+Tribonacci(n-2)+Tribonacci(n-3);;
-		return elementosTribonacci[n];
-	}
-	
+  if (n == 2)			//Condicional que evalua los casos base anteriores
+    {
+      elementosTribonacci[n] = 2;
+      return elementosTribonacci[n];
+    }
+
+  if (elementosTribonacci[n] != 0)
+    {
+      return elementosTribonacci[n];
+    }
+
+  else
+    {
+      elementosTribonacci[n] =
+	Tribonacci (n - 1) + Tribonacci (n - 2) + Tribonacci (n - 3);;
+      return elementosTribonacci[n];
+    }
+
 };
 
-void wait()
+void
+wait ()
 {
-	
-	while(getchar() != '\n');
+
+  while (getchar () != '\n');
 }
 
-void menu()
+void
+menu ()
 {
-	char aux[2];
-	int n=0;
-	int opcion=0;
-	
-	do 
-	{
-	printf("Menu:\n");
-	printf("1.Fibonacci\n" );
-	printf("2.Tribonacci\n" );
-        printf("3.Salir\n" );
-	printf("Seleccione Opcion\t" );
+  char aux[2];
+  int n = 0;
+  int opcion = 0;
 
-	fgets (aux, 2, stdin);
-	sscanf (aux, "%d", &opcion);
+  do
+    {
+      printf ("Menu:\n");
+      printf ("1.Fibonacci\n");
+      printf ("2.Tribonacci\n");
+      printf ("3.Salir\n");
+      printf ("Seleccione Opcion\t");
 
-	switch(opcion) 
+      fgets (aux, 2, stdin);
+      sscanf (aux, "%d", &opcion);
+
+      switch (opcion)
 	{
-        case 1: 
-		BorrarPantalla();
-        	printf("Bienvenido a Fibonacci:\n");
-        	printf("Ingrese n a calcular :\t");
-        	scanf("%d",&n);
-        	printf ("El termino %d es: %u \n", n, Fibonacci(n));
-            break; 
-        case 2:
-		BorrarPantalla();
-        	printf("Bienvenido a Tribonacci:\n");
-        	printf("Ingrese n a calcular :\t");
-        	scanf("%d",&n);
-        	printf ("El termino %d es: %u \n", n, Tribonacci(n));
-            break; 
-	case 3:	
-		 printf("Bye :D\n");
-        default: printf("Error"); 
+	case 1:
+	  BorrarPantalla ();
+	  printf ("Bienvenido a Fibonacci:\n");
+	  printf ("Ingrese n a calcular :\t");
+	  scanf ("%d", &n);
+	  printf ("El termino %d es: %u \n", n, Fibonacci (n));
+	  break;
+	case 2:
+	  BorrarPantalla ();
+	  printf ("Bienvenido a Tribonacci:\n");
+	  printf ("Ingrese n a calcular :\t");
+	  scanf ("%d", &n);
+	  printf ("El termino %d es: %u \n", n, Tribonacci (n));
+	  break;
+	case 3:
+	  printf ("Bye :D\n");
+	default:
+	  printf ("Error");
 
 	}
 
-	wait();
+      wait ();
 
-	}while(opcion!=3);
-	
-		
+    }
+  while (opcion != 3);
+
+
 }
 
 // Raiz del programa
-int main(void)
+int
+main (void)
 {
-	
-	int i=2;
-	int j=3;
-	
 
-	
-	while(i<1000)
-	{
-		elementosFibonacci[i]=0;
-		i++;
-	}
+  int i = 2;
+  int j = 3;
 
-	while(j<1000)
-	{
-		elementosTribonacci[i]=0;
-		j++;
-	}
 
-	menu();
 
-	
+  while (i < 1000)
+    {
+      elementosFibonacci[i] = 0;
+      i++;
+    }
+
+  while (j < 1000)
+    {
+      elementosTribonacci[i] = 0;
+      j++;
+    }
+
+  menu ();
+
+
 }
-	
-	
